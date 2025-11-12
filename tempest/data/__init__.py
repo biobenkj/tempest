6237e7a548e1391c4ccc837a6ffd4934689b8db0
biobenkj <ben.johnson4343@gmail.com>
2025-11-11 23:19:04 -0500

    update simulator init

--- tempest/data/__init__.py
+++ tempest/data/__init__.py
@@ -4,11 +4,28 @@ Data module for Tempest.
 Contains data simulation, preprocessing, and generators.
 """
 
-from .simulator import SequenceSimulator, create_simulator_from_config
+from .simulator import (
+    SimulatedRead,
+    TranscriptPool,
+    WhitelistManager,
+    PolyATailGenerator,
+    ErrorSimulator,
+    SequenceSimulator,
+    create_simulator_from_config,
+    reads_to_arrays,
+    demonstrate_probabilistic_generation
+    )
 from .invalid_generator import InvalidReadGenerator
 
 __all__ = [
-    "SequenceSimulator",
-    "InvalidReadGenerator",
-    "create_simulator_from_config",
+    'SimulatedRead',
+    'TranscriptPool',
+    'WhitelistManager',
+    'PolyATailGenerator',
+    'ErrorSimulator',
+    'SequenceSimulator',
+    'create_simulator_from_config',
+    'reads_to_arrays',
+    'demonstrate_probabilistic_generation',
+    'InvalidReadGenerator',
 ]
